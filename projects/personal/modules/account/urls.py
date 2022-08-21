538adff512ae0914a766c51590901d87adf4f95b
Weps <weaponz326@gmail.com>
2022-08-21 14:20:42 +0000

    accounts module models and views

--- projects/personal/modules/account/urls.py
+++ projects/personal/modules/account/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-
+    path('account/', views.AccountView.as_view()),
+    path('account/<id>', views.AccountDetailView.as_view()),
+    path('transaction/', views.TransactionView.as_view()),
+    path('transaction/<id>', views.TransactionDetailView.as_view()),
+    path('all-transactions/', views.AllTransactionsView.as_view()),
 ]
--- projects/personal/modules/account/views.py
+++ projects/personal/modules/account/views.py
@@ -1,3 +1,103 @@
 from django.shortcuts import render
+from django.db.models import Count
+from django.db.models.functions import TruncDate
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.filters import OrderingFilter
+
+from .models import Account, Transaction
+from .serializers import AccountSerializer, TransactionSerializer
+from users.paginations import TablePagination
+
 
 # Create your views here.
+
+class AccountView(APIView, TablePagination):
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['account_name', 'account_number', 'bank_name']
+    ordering = ['-pkid']
+
+    def get(self, request, format=None):
+        user = self.request.query_params.get('user', None)
+        account = Account.objects.filter(user=user)
+        results = self.paginate_queryset(account, request, view=self)
+        serializer = AccountSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class AccountDetailView(APIView):
+    def get(self, request, id, format=None):
+        account = Account.objects.get(id=id)
+        serializer = AccountSerializer(account)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        account = Account.objects.get(id=id)
+        serializer = AccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, id, format=None):
+        account = Account.objects.get(id=id)
+        account.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# ---------------------------------------------------------------------------------------------------------
+# transactions
+
+class TransactionView(APIView):
+    def get(self, request, format=None):
+        account = self.request.query_params.get('account', None)
+        transaction = Transaction.objects.filter(account=account)
+        serializer = TransactionSerializer(transaction, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TransactionSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class TransactionDetailView(APIView):
+    def get(self, request, id, format=None):
+        transaction = Transaction.objects.get(id=id)
+        serializer = TransactionSerializer(transaction)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        transaction = Transaction.objects.get(id=id)
+        serializer = TransactionSerializer(transaction, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, id, format=None):
+        transaction = Transaction.objects.get(id=id)
+        transaction.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# all transactions
+class AllTransactionsView(APIView, TablePagination):
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['transaction_date', 'account__account_name', 'account__bank_name', 'description', 'transaction_type', 'amount']
+    ordering = ['-pkid']
+
+    def get(self, request, format=None):
+        user = self.request.query_params.get('user', None)
+        account = Transaction.objects.filter(account__user=user)
+        results = self.paginate_queryset(account, request, view=self)
+        serializer = TransactionSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
