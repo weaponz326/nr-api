49b5c26dd16591855f639fdbedea39422a294478
Weps <weaponz326@gmail.com>
2022-08-21 13:56:05 +0000

    budget module models and views

--- projects/personal/modules/budget/urls.py
+++ projects/personal/modules/budget/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 
 urlpatterns = [
-
+    path('budget/', views.BudgetView.as_view()),
+    path('budget/<id>', views.BudgetDetailView.as_view()),
+    path('income/', views.IncomeView.as_view()),
+    path('income/<id>', views.IncomeDetailView.as_view()),
+    path('expenditure/', views.ExpenditureView.as_view()),
+    path('expenditure/<id>', views.ExpenditureDetailView.as_view()),
 ]
--- projects/personal/modules/budget/views.py
+++ projects/personal/modules/budget/views.py
@@ -1,3 +1,126 @@
 from django.shortcuts import render
+from django.db.models import Count
+from django.db.models.functions import TruncDate
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.filters import OrderingFilter
+
+from .models import Budget, Income, Expenditure
+from .serializers import BudgetSerializer, IncomeSerializer, ExpenditureSerializer
+from users.paginations import TablePagination
+
 
 # Create your views here.
+
+class BudgetView(APIView, TablePagination):
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['budget_name', 'budget_type']
+    ordering = ['-pkid']
+
+    def get(self, request, format=None):
+        user = self.request.query_params.get('user', None)
+        budget = Budget.objects.filter(user=user)
+        results = self.paginate_queryset(budget, request, view=self)
+        serializer = BudgetSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BudgetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class BudgetDetailView(APIView):
+    def get(self, request, id, format=None):
+        budget = Budget.objects.get(id=id)
+        serializer = BudgetSerializer(budget)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        budget = Budget.objects.get(id=id)
+        serializer = BudgetSerializer(budget, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, id, format=None):
+        budget = Budget.objects.get(id=id)
+        budget.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# income
+# --------------------------------------------------------------------------------------------------
+
+class IncomeView(APIView):
+    def get(self, request, format=None):
+        budget = self.request.query_params.get('budget', None)
+        income = Income.objects.filter(budget=budget)
+        serializer = IncomeSerializer(income, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = IncomeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class IncomeDetailView(APIView):
+    def get(self, request, id, format=None):
+        income = Income.objects.get(id=id)
+        serializer = IncomeSerializer(income)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        income = Income.objects.get(id=id)
+        serializer = IncomeSerializer(income, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, id, format=None):
+        income = Income.objects.get(id=id)
+        income.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# income
+# --------------------------------------------------------------------------------------------------
+
+class ExpenditureView(APIView):
+    def get(self, request, format=None):
+        budget = self.request.query_params.get('budget', None)
+        expenditure = Expenditure.objects.filter(budget=budget)
+        serializer = ExpenditureSerializer(expenditure, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ExpenditureSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class ExpenditureDetailView(APIView):
+    def get(self, request, id, format=None):
+        expenditure = Expenditure.objects.get(id=id)
+        serializer = ExpenditureSerializer(expenditure)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        expenditure = Expenditure.objects.get(id=id)
+        serializer = ExpenditureSerializer(expenditure, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, id, format=None):
+        expenditure = Expenditure.objects.get(id=id)
+        expenditure.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
